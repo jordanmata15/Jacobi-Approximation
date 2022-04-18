81b41e09d06955a7b0363b3413bb14e785b3edce
Jordan Mata <jordanmata15@gmail.com>
2022-04-18 11:53:09 -0700

    Formatting changes.

--- scripting/plot.py
+++ scripting/plot.py
@@ -11,6 +11,7 @@ curr_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.join(os.path.join(curr_dir, ".."), "data")
 file_path = os.path.join(data_dir, 'output.csv')
 
+
 def plot_2d(output_df):
     """ Plot a square 2D dataframe as a 2D heatmap.
 
@@ -22,10 +23,11 @@ def plot_2d(output_df):
     ax = sns.heatmap(output_df, cmap=cm.coolwarm)
     plt.show()
 
+
 def plot_3d(output_df):
     """ Plot a square 2D dataframe as a 3D heatmap. 
         The values at index i,j are the Z values.
-    
+
     :param: output_df - The 2D dataframe to plot.
     """
     x = y = np.arange(0, 12, 1)
@@ -34,13 +36,12 @@ def plot_3d(output_df):
 
     fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
     ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-    ax.view_init(20, -130) # rotate the plot for best viewing angle
+                    linewidth=0, antialiased=False)
+    ax.view_init(20, -130)  # rotate the plot for best viewing angle
     plt.show()
 
-if __name__=='__main__':
-    output_df=pd.read_csv(file_path, header=None)
+
+if __name__ == '__main__':
+    output_df = pd.read_csv(file_path, header=None)
     plot_2d(output_df)
     plot_3d(output_df)
-
-    
